52a8289642e72443507fa7d274b079ecb05b25c7
Wes021 <122152471+Wes021@users.noreply.github.com>
2025-02-08 13:30:47 +0300

    half done

--- LMS/app/Http/Controllers/SuperAdminControllers/UserManagament.php
+++ LMS/app/Http/Controllers/SuperAdminControllers/UserManagament.php
@@ -3,9 +3,17 @@
 namespace App\Http\Controllers\SuperAdminControllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserManagament extends Controller
 {
-    
+    public function EditUserForm(Request $request){
+        $user=User::table('user')
+        ->leftjoin('user_status','user.status','=','user_status.user_status_id')
+        ->select('user.*', 'user_status.user_status as user_status')
+        ->get();
+
+        return view('','');
+    }
 }
